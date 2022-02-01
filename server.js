d830f8782c907f050a87e1e8831053e9b8664285
sinka27 <sinka27392@gmail.com>
2022-01-31 23:24:02 -0800

    1.  Point Server to the route files 2.  Create an express server 3. Parse incoming string/array data and JSON data 4. GET Route for homepage

---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+const express = require('express');
+
+const path = require('path');
+// Point Server to the route files
+const api = require('./routes/htmlRoutes.js');
+
+// Create an express server
+const app = express();
+
+
+// Set PORT
+const PORT = 3001;
+
+// Parse incoming string or array data
+app.use(express.urlencoded({extended:true}));
+// Parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Listener
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT} 🚀`);
+});
