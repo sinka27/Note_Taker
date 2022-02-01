6aa8eb8183959cbd74fac0cb77cc6c26a766a39c
sinka27 <sinka27392@gmail.com>
2022-01-31 23:20:11 -0800

    POST Route for submitting a note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,6 +12,27 @@ apiRouter.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
+// POST Route for submitting a note
+apiRouter.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (req.body) {
+    // Variable for the object will save
+    const newNote = {
+        title,
+        text,
+        id: uuid()+uuid(),
+    };
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.json('Error in adding new Note');
+  }
+});
+
+
 
 
 module.exports = apiRouter;
